f7056dfdf43852f91fbe22fee9e0ac52d5194dce
Shawn Sesna <shawn.sesna@octopus.com>
2021-05-20 07:47:42 -0700

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,6 @@ ENV['RACK_ENV'] ||= 'development'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-#ActiveRecord::Base.establish_connection(
-#  ENV['DATABASE_URL'] || { adapter: 'postgresql',
-#                           database: "veggie_tracker",
-#                           username: "postgres",
-#                           password: "GrassGrows2!",
-#                           host: "clusterpi-1",
-#                           port: 5432}
-#)
-
 require 'dotenv'
 Dotenv.load
 
